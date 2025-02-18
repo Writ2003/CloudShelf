8c8f0e1e5c85f73efa7b2d5b58a485119baec144
Writ2003 <dassupratim57@gmail.com>
2025-02-19 01:36:37 +0530

    ResponsivenessAdded_as_on_19/02/2025_by_SD

--- src/componenets/Sidebar.jsx
+++ src/componenets/Sidebar.jsx
@@ -5,8 +5,8 @@ import CloudShelfLogo from '/src/assets/CloudShelf_logo_small.png?url';
 
 const Sidebar = () => {
   return (
-    <div className="w-64 bg-white shadow-xl h-full p-4 rounded-l-xl">
-      <div className="text-2xl font-bold mb-10 flex gap-1 items-center">
+    <div className="w-44 lg:w-64 bg-white shadow-xl h-full p-1.5 lg:p-4 rounded-l-xl">
+      <div className="text-lg lg:text-2xl font-bold mb-10 flex gap-1 items-center">
         <img src={CloudShelfLogo} height={60} width={60}/>
         <span className='font-extrabold'>Cloud</span>Shelf
       </div>
